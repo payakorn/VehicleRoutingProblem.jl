94852b8b48605ef7196d993715d5201cd70889ac
Payakorn Saksuriya <payakornn@gmail.com>
2022-03-02 01:41:56 +0700

    add files and test

--- src/VehicleRoutingProblems.jl
+++ src/VehicleRoutingProblems.jl
@@ -1,5 +1,14 @@
 module VehicleRoutingProblems
 
-# Write your package code here.
+using Glob, Printf, Combinatorics, JSON, TimerOutputs, Dates, PrettyTables, DelimitedFiles, CSV, VegaLite, DataFrames, Statistics, Plots, Clustering, Random
+
+# include files
+include("read_file.jl")
+include("heuristic.jl")
+include("benchmark.jl")
+include("run_benchmark.jl")
+include("ParticleSwarm.jl")
+
+export read_txt, load_all_data, read_txt3
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "VehicleRoutingProblems.jl" begin
     # Write your tests here.
+    println(pwd())
+    vehicle = read_txt3(joinpath(@__DIR__, "..", "particle_swarm", "total_distance", "case16", "c101", "15", "1", "c101-1.txt"), "c101")
+    @test floor(vehicle["TotalDistance"], digits=2) == 828.93
 end
