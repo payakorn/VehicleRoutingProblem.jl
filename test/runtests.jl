7f10a51f1fa20dc3e5fb9be9a8b9bb64d3d74d83
Payakorn Saksuriya <payakornn@gmail.com>
2022-03-05 14:06:52 +0700

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,6 @@ using Test
 
 @testset "VehicleRoutingProblems.jl" begin
     # Write your tests here.
-    @test total_route([0, 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 0]) == 3
+    @test 1 == 1
+    @test VehicleRoutingProblems.total_route([0, 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 0]) == 3
 end
